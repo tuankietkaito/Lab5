70a8bed31a2dfa3b4d4e29e7f2b8ec01d0c921d8
tuankietkaito <tuankietkaito@gmail.com>
2020-05-19 10:34:04 +0700

    lab5

--- ex1.c
+++ ex1.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <math.h>
 
-int count;
+int part = 0;
+long count[3] = {0};
 void *letCount(void *param);
 
 int main(int argc, char *argv[])
 {
     pthread_t tid[3];
-    
+
     for (int i = 0; i < 4; i++)
     {
         pthread_create(&tid[i], NULL, letCount, atoi(argv[1]) / 4);
@@ -17,12 +18,18 @@ int main(int argc, char *argv[])
     {
         pthread_join(tid[i], NULL);
     }
-    printf("%d\n", count);
+    int total = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        total += count[i];
+    }
+    printf("%d\n", 4 * total / (atoi(argv[1])));
     return 0;
 }
 
 void *letCount(void *param)
 {
+    int k = part++;
     srand(time);
     int n = atoi(param);
     for (int i = 0; i < n; i++)
@@ -30,7 +37,7 @@ void *letCount(void *param)
         double x = (double)rand() / RAND_MAX * 2.0 - 1.0;
         double y = (double)rand() / RAND_MAX * 2.0 - 1.0;
         if ((double)sqrt(pow(x, 2) + pow(y, 2)) <= 1.0)
-            count++;
+            count[k]++;
     }
     pthread_exit(0);
 }
